504f06e2d365a820bbd3d908c04910cffe3b1dbe
eatmeatball <abandon1a2b@outlook.com>
2024-09-30 18:13:42 +0800

    main:clean

--- main.go
+++ main.go
@@ -14,43 +14,11 @@ func init() {
 }
 
 func main() {
-	//if _, err := os.Stat("jobConfig.json"); os.IsNotExist(err) {
-	//	fmt.Println("当前目录下不存在jobConfig.json文件")
-	//
-	//	// 询问是否生成该文件
-	//	fmt.Print("是否生成jobConfig.json文件？(yes/no): ")
-	//	var answer string
-	//	_, err := fmt.Scanln(&answer)
-	//	if err != nil {
-	//		fmt.Println("无法读取输入，错误：", err)
-	//		return
-	//	}
-	//
-	//	if answer == "yes" {
-	//		err = os.WriteFile("jobConfig.json", resource.GetJobConfigDefault(), 0644)
-	//		if err != nil {
-	//			fmt.Println("无法写入文件，错误：", err)
-	//			return
-	//		}
-	//		fmt.Println("jobConfig.json文件已生成并写入内容。请调整配置后再次启动")
-	//	} else {
-	//		fmt.Println("请补充 jobConfig.json 后再次启动程序")
-	//	}
-	//	return
-	//}
-	//fileData, err := os.ReadFile("jobConfig.json")
-	//if err != nil {
-	//	slog.Error(err.Error())
-	//	return
-	//}
-
 	err := jobmanager.RegByUserConfig()
 	if err != nil {
 		slog.Error(err.Error())
 		return
 	}
-
-	// jobmanager.Reg(fileData)
 	jobmanagerserver.ServeRun()
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
